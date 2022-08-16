e80fed8461c5b134c82a089cd14e49e7491292ed
MarcoSchiavello <marco.schiavello4@gmail.com>
2022-08-16 22:31:50 +0200

    removed debug code

--- src/implc/minimax.cpp
+++ src/implc/minimax.cpp
@@ -1,20 +1,8 @@
 #include "../implh/minimax.h"
 
-#define OPTIM
-//#define DEBUG
-
 evalRecord_t minimax(char** field, int high, int alpha, int beta, bool maximize) {
-    #ifdef DEBUG
-        char input;
-    #endif
-
     int eval = checkWinner(field);
     if((findNthBlankSpace(field).row == -1) || eval != 0 || !high) {
-        #ifdef DEBUG
-            printf("eval res: %d\n", eval);
-            scanf("%c", &input); 
-        #endif
-
         return (evalRecord_t) {
             .value = eval == 2 ? 1 : (eval == 1 ? -1 : 0) ,
             .actionPosition = { -1, -1 }
@@ -24,47 +12,26 @@ evalRecord_t minimax(char** field, int high, int alpha, int beta, bool maximize)
     int childNum = 0;
     evalRecord_t maxEvalRecord = (evalRecord_t){ INT_MIN, { -1, -1 } }, minEvalRecord = (evalRecord_t){ INT_MAX, { -1, -1 } };
     position_t nextPos;
+
     while((nextPos = findNthBlankSpace(field, ++childNum)).row != -1) {
         field[nextPos.row][nextPos.col] = (maximize * 'O') + (!maximize * 'X');
-
-        #ifdef DEBUG
-            printf("|| childNum: %d - high: %d | role: %s\n", childNum, high, maximize ? "true" : "false");
-            drawField(field);
-            
-            scanf("%c", &input);
-        #endif
 
         evalRecord_t evalRecord = minimax(field, high - 1, alpha, beta, !maximize);
         evalRecord.actionPosition = (position_t){ nextPos.row, nextPos.col };
 
-        #ifdef DEBUG
-            printf("child res{ value: %d || row: %d || col: %d }\n", evalRecord.value, evalRecord.actionPosition.row, evalRecord.actionPosition.col);
-
-            scanf("%c", &input);
-        #endif
-
         maxEvalRecord = (evalRecord.value > maxEvalRecord.value) ? evalRecord : maxEvalRecord;
         minEvalRecord = (evalRecord.value < minEvalRecord.value) ? evalRecord : minEvalRecord;
 
-        #ifdef OPTIM
-            if(maximize)
-                alpha = alpha > evalRecord.value ? alpha : evalRecord.value;
-            else
-                beta = beta > evalRecord.value ? evalRecord.value : beta;
-        #endif
+        if(maximize)
+            alpha = alpha > evalRecord.value ? alpha : evalRecord.value;
+        else
+            beta = beta > evalRecord.value ? evalRecord.value : beta;
 
         field[nextPos.row][nextPos.col] = ' ';
 
-        #ifdef OPTIM
-            if(beta <= alpha)
-                break;
-        #endif
+        if(beta <= alpha)
+            break;
     }
-
-    #ifdef DEBUG
-        printf("END CHILDREN LIST\n\n");
-        scanf("%c", &input);
-    #endif
 
     return maximize ? maxEvalRecord : minEvalRecord;
 } 
@@ -73,11 +40,6 @@ position_t findNthBlankSpace(char** field, int blankNum) {
     for(int i = 0; i < FIELD_DIM; i++)
         for(int j = 0; j < FIELD_DIM; j++)
             if(field[i][j] == ' ') {
-
-                #ifdef DEBUG
-                    printf("current blank{ row: %d || col: %d || blankLeft: %d }\n", i, j, blankNum);
-                #endif
-
                 blankNum--;
                 if(blankNum <= 0)
                     return (position_t) {
@@ -98,30 +60,7 @@ position_t findNthBlankSpace(char** field, int blankNum) {
 *       @return void
 */
 void AIpick(char** field) {
-
-    #ifdef DEBUG
-        for(int i = 0; i < FIELD_DIM; i++)
-            for(int j = 0; j < FIELD_DIM; j++)
-                field[i][j] = ' ';
-
-        field[0][0] = 'O';
-        field[0][1] = ' ';
-        field[0][2] = 'X';
-
-        field[1][0] = ' ';
-        field[1][1] = ' ';
-        field[1][2] = ' ';
-
-        field[2][0] = 'O';
-        field[2][1] = ' ';
-        field[2][2] = 'X';
-    #endif
-
     evalRecord_t res = minimax(field, INT_MAX, INT_MIN, INT_MAX);
-
-    #ifdef DEBUG
-        printf("optimal solution{ row: %d || col: %d |||| value:  %d }\n", res.actionPosition.row, res.actionPosition.col, res.value);
-    #endif
 
     field[res.actionPosition.row][res.actionPosition.col] = 'O';
 }
